808f58de39c39941ec742d0a3c43683c34ab6e45
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-11 00:00:35 +0530

    Added GCK configs

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,8 +4,10 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/users/currentuser", (req, res) => {
+app.get("/api/users/currentuser", (_, res) => {
   res.send("Hi there!");
 });
 
-app.listen(3000, () => console.log("Auth service listening at port 3000.."));
+app.listen(3000, () => {
+  console.log("Auth service listening at port 3000...");
+});
