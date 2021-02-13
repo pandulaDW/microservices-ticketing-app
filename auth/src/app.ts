d1b11f1db0d3be03aeb93ccb41930dbfa04cb5d6
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-13 14:06:59 +0530

    Finished test setup

---
+++ auth/src/app.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import cookieSession from "cookie-session";
+import "express-async-errors";
+import { currentUserRouter } from "./routes/current-user";
+import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { SignInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+
+const app = express();
+app.set("trust proxy", true);
+
+// middlewares
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+// routers
+app.use("/api/users/signup", signUpRouter);
+app.use("/api/users/signin", SignInRouter);
+app.use("/api/users/signout", signOutRouter);
+app.use("/api/users/currentuser", currentUserRouter);
+
+// global error handler
+app.use(errorHandler);
+
+export { app };
---
+++ auth/src/test/setup.ts
@@ -0,0 +1,33 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+
+// mongodb instance
+let mongo: MongoMemoryServer;
+
+// create a new mongo instance before all tests
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+});
+
+// clearing data of each collection before each test
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// stopping the db and closing connections after tests
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
