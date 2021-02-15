cff19b929625096c3b2a4dd091409a166bdcf640
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-15 23:18:03 +0530

    Added a userequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,15 +1,57 @@
+import axios from "axios";
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label htmlFor="email">Email Address</label>
-        <input type="text" className="form-control" name="email" />
+        <input
+          type="text"
+          className="form-control"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="form-group">
         <label htmlFor="password">Password Address</label>
-        <input type="password" className="form-control" name="password" />
+        <input
+          type="password"
+          className="form-control"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Ooops...</h4>
+          <ul className="my-0">
+            {errors.map((err, id) => (
+              <li key={id}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
