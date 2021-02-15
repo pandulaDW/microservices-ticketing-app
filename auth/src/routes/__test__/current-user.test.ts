c62cd17bc8389f231a22e6d5ce2bf45454b8c282
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-15 11:04:00 +0530

    Added next js to the project

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -12,3 +12,12 @@ it("responds with details about the current user", async () => {
 
   expect(response.body.currentUser.email).toEqual(global.data.email);
 });
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toBeNull();
+});
