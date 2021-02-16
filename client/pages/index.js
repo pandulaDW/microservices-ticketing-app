f1675d8407cd577ce8c560b5db1eeb902cf31a9f
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-16 12:22:34 +0530

    Connected get initial props to kubectl cluster

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,26 @@
-const Home = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page Mate</h1>;
 };
 
-export default Home;
+LandingPage.getInitialProps = async () => {
+  // check if env is server or client
+  if (typeof window === "undefined") {
+    const response = await axios.get(
+      "http://nginx-ingress-controller.kube-system.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+    return response.data;
+  } else {
+    const response = await axios.get("/api/users/currentuser");
+    return response.data;
+  }
+};
+
+export default LandingPage;
