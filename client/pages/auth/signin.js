ec4d7c82a49673bd82148734ec5e330420e21a8f
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-19 17:57:11 +0530

    Added signIn component

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Router from "next/router";
+import useRequest from "../../hooks/use-request";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signin",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+    onSuccess: () => Router.push("/"),
+  });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <h1>Sign In</h1>
+      <div className="form-group">
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="text"
+          className="form-control"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password Address</label>
+        <input
+          type="password"
+          className="form-control"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      {errors}
+      <button className="btn btn-primary">Sign In</button>
+    </form>
+  );
+};
+
+export default SignIn;
