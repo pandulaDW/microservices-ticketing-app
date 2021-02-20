9a271590d7daa80bb672f5cce3cb9ac20565f9f2
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-20 10:28:13 +0530

    Added get initial props to the _app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,22 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+App.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  const pageProps = data;
+
+  return { pageProps };
 };
 
 export default App;
