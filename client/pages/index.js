24b396a9ed71d9aa0d3f90a8945a45578683f4fa
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-20 16:23:31 +0530

    Added a signout component

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,7 @@ const LandingPage = ({ currentUser }) => {
   return currentUser ? (
     <h1>You are signed in</h1>
   ) : (
-    <h1>You are not signed in</h1>
+    <h1>You are NOT signed in</h1>
   );
 };
 
