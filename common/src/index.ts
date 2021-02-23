9a4fb0795a3e7b98d55e0322d0a6153a7c6a9c67
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-02-23 19:33:28 +0530

    Added config settings

---
+++ common/src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  blue: 10,
+  green: 10,
+  red: 10,
+};
+
+console.log(color);
+
+export default color;
